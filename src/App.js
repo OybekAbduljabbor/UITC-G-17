3f5cd7a8a4692679fb74f31ad244a40f2fba22e3
OybekAbduljabbor <abdujabborovoybek@gmail.com>
2022-07-16 10:55:14 +0500

    70 darsni joyladim rasim qoshish uchun ajoib functional dastur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "./App.css";
-import { AddImg } from "./Pages/AddImg/AddImg";
 
 export function App() {
-  return (
-    <>
-      <AddImg />
-    </>
-  );
+  return <></>;
 }
